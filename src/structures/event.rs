c6be541d0d412f83d3c62689c9be42c09723fa96
AlexPresso <blond.alexandre@hotmail.com>
2024-01-29 17:00:07 +0100

    first commit

---
+++ src/structures/event.rs
@@ -0,0 +1,19 @@
+use crate::structures::card::Item;
+use crate::structures::pack::Pack;
+
+#[serde(rename_all = "camelCase")]
+pub struct Event {
+    pub id: String,
+    pub name: String,
+    pub image_url: String,
+    pub begin_date: String,
+    pub end_date: String,
+    pub balance_cost: u16,
+    pub lore_dust_cost: u16,
+    pub invoke_count: u16,
+    pub is_active: bool,
+    pub is_one_time: bool,
+    pub is_pity_enabled: bool,
+    pub pack: Pack,
+    pub items: Vec<Item>
+}
---
+++ src/structures/mod.rs
@@ -0,0 +1,2 @@
+pub(crate) mod pack;
+mod card;
